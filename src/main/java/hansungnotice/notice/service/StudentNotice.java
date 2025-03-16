781b921955d72f19913b78b4cab5f5c74f0852d6
Dongnyoung <leedo7182@naver.com>
2025-03-16 18:08:31 +0900

    학사공지 추가

--- src/main/java/hansungnotice/notice/service/StudentNotice.java
+++ src/main/java/hansungnotice/notice/service/StudentNotice.java
@@ -2,7 +2,6 @@ package hansungnotice.notice.service;
 
 import java.io.IOException;
 import java.time.LocalDate;
-import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.List;
 
