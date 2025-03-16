761c6bf7f6cfaad295b6ccb0fed0400c28a28cb0
Dongnyoung <leedo7182@naver.com>
2025-03-16 18:09:04 +0900

    학사공지 추가

--- src/main/java/hansungnotice/notice/service/StudentNotice.java
+++ src/main/java/hansungnotice/notice/service/StudentNotice.java
@@ -24,10 +24,10 @@ public class StudentNotice {
     }
 
 
-    private final String url = "https://www.hansung.ac.kr/hansung/8385/subview.do?enc=Zm5jdDF8QEB8JTJGYmJzJTJGaGFuc3VuZyUyRjE0MyUyRmFydGNsTGlzdC5kbyUzRmJic0NsU2VxJTNEMjM2JTI2YmJzT3BlbldyZFNlcSUzRCUyNmlzVmlld01pbmUlM0RmYWxzZSUyNnNyY2hDb2x1bW4lM0RzaiUyNnNyY2hXcmQlM0QlMjY%3D";
     @Transactional
      public void fetchAndSaveNotices(){
         try{
+            String url = "https://www.hansung.ac.kr/hansung/8385/subview.do?enc=Zm5jdDF8QEB8JTJGYmJzJTJGaGFuc3VuZyUyRjE0MyUyRmFydGNsTGlzdC5kbyUzRmJic0NsU2VxJTNEMjM2JTI2YmJzT3BlbldyZFNlcSUzRCUyNmlzVmlld01pbmUlM0RmYWxzZSUyNnNyY2hDb2x1bW4lM0RzaiUyNnNyY2hXcmQlM0QlMjY%3D";
             Document doc = Jsoup.connect(url).get();
 
             // form[name=viewForm] 내에서 board-table horizon1 클래스를 찾고, tbody 안의 tr의 td.td-subject 선택
