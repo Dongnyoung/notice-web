0fb492684c32717d5ba486d3d6b2c8d28009c434
Dongnyoung <leedo7182@naver.com>
2025-04-05 14:19:39 +0900

    서버재설정

--- src/main/java/hansungnotice/notice/config/WebConfig.java
+++ src/main/java/hansungnotice/notice/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")  // 모든 경로에 대해 CORS 설정
-                .allowedOrigins("chrome-extension://apcgkcekbhhjdddlmlfehfomdlnpdeek","http://3.35.16.2:8080")  // 크롬 익스텐션 ID를 명시
+                .allowedOrigins("chrome-extension://apcgkcekbhhjdddlmlfehfomdlnpdeek","http://15.164.104.1468080")  // 크롬 익스텐션 ID를 명시
                 .allowedMethods("GET", "POST", "PUT", "DELETE")  // 허용할 HTTP 메서드
                 .allowedHeaders("*")  // 모든 헤더 허용
                 .allowCredentials(true);  // 쿠키 전송 허용
